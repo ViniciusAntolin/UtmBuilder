3d59aba88a1135c515713be1859aea0f33554b7d
Vinicius Pires Antolin <viniciuspantolin4@gmail.com>
2024-02-13 23:33:28 -0300

    Add project files.

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,10 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core
+{
+    public class Utm
+    {
+        public Url Url { get; set; }
+        public Campanha Campanha { get; set; }
+    }
+}
---
+++ UtmBuilder.Core/ValueObjects/Campanha.cs
@@ -0,0 +1,55 @@
+﻿namespace UtmBuilder.Core.ValueObjects
+{
+    public class Campanha : ValueObject
+    {
+        /// <summary>
+        /// Gera uma nova campanha para uma URL.
+        /// </summary>
+        /// <param name="source">O referenciador (por exemplo, google, newsletter).</param>
+        /// <param name="medium">Médio de marketing (por exemplo, cpc, banner, email).</param>
+        /// <param name="name">Produto, código promocional ou slogan (por exemplo, spring_sale). Um dos nomes da campanha ou identificador da campanha é obrigatório.</param>
+        /// <param name="id">O ID da campanha de anúncios.</param>
+        /// <param name="term">Identifica as palavras-chave pagas.</param>
+        /// <param name="content">Usado para diferenciar anúncios.</param>
+
+        public Campanha(string source, string medium, string name, string? id = null, string? term = null, string? content = null)
+        {
+            Source = source;
+            Medium = medium;
+            Name = name;
+            Id = id;
+            Term = term;
+            Content = content;
+        }
+
+        /// <summary>
+        /// O ID da campanha de anúncios.
+        /// </summary>
+        public string? Id { get; }
+
+        /// <summary>
+        /// O referenciador (por exemplo, google, newsletter).
+        /// </summary>
+        public string Source { get; }
+
+        /// <summary>
+        /// Médio de marketing (por exemplo, cpc, banner, email).
+        /// </summary>
+        public string Medium { get; }
+
+        /// <summary>
+        /// Produto, código promocional ou slogan (por exemplo, spring_sale). Um dos nomes da campanha ou identificador da campanha é obrigatório.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Identifica as palavras-chave pagas.
+        /// </summary>
+        public string? Term { get; }
+
+        /// <summary>
+        /// Usado para diferenciar anúncios.
+        /// </summary>
+        public string? Content { get; }
+    }
+}
