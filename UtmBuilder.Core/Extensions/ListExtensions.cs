5720837694510be0e14d86a2d45c5b55dc0b4736
Vinicius Pires Antolin <viniciuspantolin4@gmail.com>
2024-02-18 23:55:46 -0300

    improve: adiciona novo metodo e conclui implementacao da utm

---
+++ UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace UtmBuilder.Core.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void AddSeNaoNulo(this List<string> list, string key, string? value)
+        {
+            if (!string.IsNullOrEmpty(value))
+                list.Add($"{key}={value}");
+        }
+
+        public static string ObterParametros(this IEnumerable<string> parametros, string prefixo)
+        {
+            return parametros.Where(x => x.StartsWith(prefixo)).FirstOrDefault("").Split('=')[1];
+        }
+    }
+}
--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,4 +1,8 @@
-﻿using UtmBuilder.Core.ValueObjects;
+﻿using System.ComponentModel.Design;
+using System.Text;
+using UtmBuilder.Core.Extensions;
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core
 {
@@ -18,5 +22,39 @@ namespace UtmBuilder.Core
         /// Detalhes da campanha
         /// </summary>
         public Campanha Campanha { get; init; } // permite que apenas no construtor seja setado valor
+
+        public static implicit operator string(Utm utm) => utm.ToString();
+        public static implicit operator Utm(string link)
+        {
+            var url = new Url(link);
+            var segmentos = url.Address.Split('?');
+            if (segmentos.Length == 1)
+                throw new UrlInvalidaException("Nenhum segmento foi preenchido");
+            var parametros = segmentos[1].Split('&');
+
+            var source = parametros.ObterParametros("utm_source");
+            var medium = parametros.ObterParametros("utm_medium");
+            var name = parametros.ObterParametros("utm_campaign");
+            var id = parametros.ObterParametros("utm_id");
+            var term = parametros.ObterParametros("utm_term");
+            var content = parametros.ObterParametros("utm_content");
+
+            return new Utm(
+                new Url(segmentos[0]),
+                new Campanha(source, medium, name, id, term, content));
+        }
+
+        public override string ToString()
+        {
+            var segments = new List<string>();
+            segments.AddSeNaoNulo("utm_source", Campanha.Source);
+            segments.AddSeNaoNulo("utm_medium", Campanha.Medium);
+            segments.AddSeNaoNulo("utm_campaign", Campanha.Name);
+            segments.AddSeNaoNulo("utm_id", Campanha.Id);
+            segments.AddSeNaoNulo("utm_term", Campanha.Term);
+            segments.AddSeNaoNulo("utm_content", Campanha.Content);
+
+            return $"{Url.Address}?{string.Join("&", segments)}";
+        }
     }
 }
